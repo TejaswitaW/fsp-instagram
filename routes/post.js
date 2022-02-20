be73cd4d53111abad3e339c75c9a200a0f3b9054
Tejaswita Wakhure <tejaswitawakhure@gmail.com>
2022-02-20 17:58:37 +0530

    route handler to get all post created

--- routes/post.js
+++ routes/post.js
@@ -6,6 +6,15 @@ const router = express.Router();
 
 const Post = mongoose.model("Post");
 
+router.get("/allpost",(req,res)=>{
+  Post.find().populate("postedBy","_id name")
+  .then(posts=>{
+    res.json({posts})
+  }).catch(err=>{
+    console.log(err)
+  })
+})
+
 router.post("/createpost",requireLogin, (req, res) => {
   const { title, body } = req.body;
   if (!title || !body) {
