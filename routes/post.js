3f76a6b2360a80f5b1733ecc51b48eb32f30e7fa
Tejaswita Wakhure <tejaswitawakhure@gmail.com>
2022-02-20 16:01:37 +0530

    createpost route created

--- routes/post.js
+++ routes/post.js
@@ -9,15 +9,20 @@ const Post = mongoose.model("Post");
 router.post("/createpost",requireLogin, (req, res) => {
   const { title, body } = req.body;
   if (!title || !body) {
-    res.status(422).json({ error: "Please add all the fields!" });
-  }
-  console.log(req.user);
-  res.send("ok");
-  // const post = new post({
-  //     title,
-  //     body,
-  //     posted
-  // })
+    return res.status(422).json({ error: "Please add all the fields!" });
+  } 
+  req.user.password = undefined
+  const post = new Post({
+      title,
+      body,
+      postedBy:req.user
+  })
+  post.save().then(result=>{
+    res.json({post:result})
+  })
+  .catch(err=>{
+    console.log(err)
+  })
 });
 
 module.exports = router;
